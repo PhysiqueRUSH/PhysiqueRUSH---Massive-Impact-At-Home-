13504bffdd8efff1d24114c88034ce529e864822
PhysiqueRUSH <physiquerush@gmail.com>
2026-08-19 00:32:19 +0200

    v2.5 · corrige netteté et cadrage du personnage accueil

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE='physiquerush-mi-v10-home-athlete-v24-final';
+const CACHE='physiquerush-mi-v11-home-athlete-v25';
 const APP_SHELL=[
-  './','./index.html','./css/app.css','./css/impact-theme.css','./css/home-v23.css','./css/home-v24.css','./css/home-footer.css','./manifest.webmanifest',
+  './','./index.html','./css/app.css','./css/impact-theme.css','./css/home-v23.css','./css/home-v24.css','./css/home-v25.css','./css/home-footer.css','./manifest.webmanifest',
   './js/app.js','./js/config.js','./js/state.js','./js/scoring.js','./js/sessionStore.js',
   './js/data/exercises.js','./js/data/core.js','./js/data/cardio.js','./js/data/tests.js','./js/data/challenges.js','./js/data/drops.js','./js/data/program.js',
   './js/engine/workout.js','./js/engine/timer.js','./js/ui/components.js','./js/ui/screens.js',
-  './assets/icons/icon-32.png','./assets/icons/icon-64.png','./assets/icons/icon-192.png','./assets/icons/icon-512.png','./assets/icons/icon-1024.png','./assets/icons/icon-maskable-192.png','./assets/icons/icon-maskable-512.png','./assets/icons/apple-touch-icon-180.png','./assets/brand/physiquerush-symbol-master.png','./assets/characters/home-athlete-v24.png'
+  './assets/icons/icon-32.png','./assets/icons/icon-64.png','./assets/icons/icon-192.png','./assets/icons/icon-512.png','./assets/icons/icon-1024.png','./assets/icons/icon-maskable-192.png','./assets/icons/icon-maskable-512.png','./assets/icons/apple-touch-icon-180.png','./assets/brand/physiquerush-symbol-master.png','./assets/characters/home-athlete-v24.png','./assets/characters/home-athlete-v25.png'
 ];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP_SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
